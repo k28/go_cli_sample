5d227d5304dd6b939d0dc3af8277a216fa856b25
Kazuya Hatano <k28@me.com>
2019-07-10 06:09:47 +0900

    go fmt

--- angopipe/preparare.go
+++ angopipe/preparare.go
@@ -1,29 +1,27 @@
 package angopipe
 
 import (
-    "crypto/aes"
-    "crypto/cipher"
-    "encoding/base64"
-    "errors"
-    "os"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"errors"
+	"os"
 )
 
 func Prepare() (cipher.AEAD, error) {
-    rawKey := os.Getenv("ANGO_KEY")
-    if rawKey == "" {
-        return nil, errors.New("Environment Variable 'ANGO_KEY' is empty.")
-    }
-    key, err := base64.StdEncoding.DecodeString(rawKey)
-    if err != nil {
-        return nil, errors.New("Decode, 'ANGO_KEY' key error.")
-    }
+	rawKey := os.Getenv("ANGO_KEY")
+	if rawKey == "" {
+		return nil, errors.New("Environment Variable 'ANGO_KEY' is empty.")
+	}
+	key, err := base64.StdEncoding.DecodeString(rawKey)
+	if err != nil {
+		return nil, errors.New("Decode, 'ANGO_KEY' key error.")
+	}
 
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return nil, err
-    }
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
 
-    return cipher.NewGCM(block)
+	return cipher.NewGCM(block)
 }
-
-
